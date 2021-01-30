c40d32c3c00bbb8a68109aa93e8bf3a791a57502
skm0619 <sin1509@naver.com>
2021-01-30 15:03:32 +0900

    first commit

---
+++ omok.c
@@ -0,0 +1,25 @@
+# include "omok.h"
+
+void game_control(void) {
+    // 게임의 승패를 결정짓는 함수
+};
+
+void gotoxy(int x, int y) {
+    // 화면상의 커서 위치를 파악하고 제어하는 함수
+    COORD pos = { x,y };
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+};
+
+void show_stone()     // 바둑돌을 놓는 함수
+{
+   수정1
+};
+
+void move_position() {
+    // 사용자가 입력하는 화살표 키에 따라 좌표 x, y값을 변경하는 함수
+};
+
+
+void show_map() {
+    //바둑판을 보여주는 함수
+};
